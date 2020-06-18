9889058e02e7c22d9365f24b0f289af5ad4c4c33
Ryan Peterson <50131983+ryanp5@users.noreply.github.com>
2020-06-18 16:38:07 +1000

    locomotion and interaction
    added locomotion/interaction and input manager from VR with Andrew tutorial

---
+++ VR puzzle game/Assets/Scripts/_Input/Scripts/ExampleListener.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+
+public class ExampleListener : MonoBehaviour
+{
+    public ButtonHandler primaryAxisClickHandler = null;
+    public AxisHandler2D primaryAxisHanlder = null;
+    public AxisHandler triggerHandler = null;
+
+    public void OnEnable()
+    {
+        primaryAxisClickHandler.OnButtonDown += PrintPrimaryButtonDown;
+        primaryAxisClickHandler.OnButtonUp += PrintPrimaryButtonUp;
+        primaryAxisHanlder.OnValueChange += PrintPrimaryAxis;
+        triggerHandler.OnValueChange += PrintTrigger;
+    }
+
+    public void OnDisable()
+    {
+        primaryAxisClickHandler.OnButtonDown -= PrintPrimaryButtonDown;
+        primaryAxisClickHandler.OnButtonUp -= PrintPrimaryButtonUp;
+        primaryAxisHanlder.OnValueChange -= PrintPrimaryAxis;
+        triggerHandler.OnValueChange -= PrintTrigger;
+    }
+
+    private void PrintPrimaryButtonDown(XRController controller)
+    {
+        print("Primary button down");
+    }
+
+    private void PrintPrimaryButtonUp(XRController controller)
+    {
+        print("Primary button up");
+    }
+
+    private void PrintPrimaryAxis(XRController controller, Vector2 value)
+    {
+        print("Primary axis: " + value);
+    }
+
+    private void PrintTrigger(XRController controller, float value)
+    {
+        print("Trigger: " + value);
+    }
+}
---
+++ VR puzzle game/Assets/Scripts/_Input/Scripts/InputManager.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+
+public class InputManager : MonoBehaviour
+{
+    public List<ButtonHandler> allButtonHandlers = new List<ButtonHandler>();
+    public List<AxisHandler> allAxisHandlers = new List<AxisHandler>();
+    public List<AxisHandler2D> allAxisHandlers2D = new List<AxisHandler2D>();
+
+    private XRController controller = null;
+    private void Awake()
+    {
+        controller = GetComponent<XRController>();
+    }
+
+    private void Update()
+    {
+        HandleButtonEvents();
+        HandleAxis2DEvents();
+        HandleAxisEvents();
+    }
+
+    private void HandleButtonEvents()
+    {
+        foreach(ButtonHandler handler in allButtonHandlers)   
+            handler.HandleState(controller);
+     
+    }
+
+    private void HandleAxis2DEvents()
+    {
+        foreach (AxisHandler2D handler in allAxisHandlers2D)
+            handler.HandleState(controller);
+    }
+
+    public void HandleAxisEvents()
+    {
+        foreach (AxisHandler handler in allAxisHandlers)
+            handler.HandleState(controller);
+    }
+}
+
